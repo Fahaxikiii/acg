61f25baf8995e6280a0667ff905b43e4b5a44e01
万里 <920113370213@njust.edu.cn>
2024-10-05 23:42:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pkg.plugin.events import *  # 导入事件类
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+@register(name="ACG", description="ACG PIC", version="0.1", author="Fahaxikiii")
 class MyPlugin(BasePlugin):
 
     # 插件加载时触发
